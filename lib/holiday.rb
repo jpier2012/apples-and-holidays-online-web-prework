24ac387df70807c050e79bd1e486942976d1083e
James Pier <jpier2012@gmail.com>
2019-04-08 19:30:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,6 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
     }
 end
 
-puts add_supply_to_winter_holidays(holiday_hash, "THE SHIT")
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
