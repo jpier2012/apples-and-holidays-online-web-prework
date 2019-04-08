d2c2db6ffff6444b88ac9e4d1eee3836fd1d2864
James Pier <jpier2012@gmail.com>
2019-04-08 20:06:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,7 +88,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each {|season, holiday|
     puts "#{season.capitalize}:"
       holiday.each {|day, supplies|
-        capital = day.split(" ")
+        capital = day.to_s.split(" ")
         capital.collect! {|word|
           word.capitalize
         }
