411d00062fa3a5c4a429c4e1155875e9339de5a3
James Pier <jpier2012@gmail.com>
2019-04-08 20:48:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -106,11 +106,9 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_list = []
   holiday_hash.select {|season, holidays|
-    holiday_list << holidays.select {|holiday, supplies|
-      supplies.include?("BBQ")
-    }
+    holiday_list << holidays.select {|holiday, supplies| supplies.include?("BBQ")}
   }
-  binding.pry
+  holiday_list.keys
 end
 
 
